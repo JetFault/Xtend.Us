f75172c937f00b3f22693c990912c176eee1c885
Jerry <jreptak@jet.(none)>
2012-03-24 18:48:52 -0400

    Set up

---
+++ likes-dislikes.php
@@ -0,0 +1,24 @@
+<?php
+
+	$like = $_POST['like'];
+	$dislike = $_POST['dislike'];
+
+	//Add to like DB
+	if(isset($like) {
+		$likes = delimit_input($like);
+		var_dump($likes);
+
+	}
+	
+
+	//Add to dislike DB
+	if(isset($dislike) {
+		$dislikes = delimit_input($dislike);
+		var_dump($dislikes);
+	}
+
+	function delimit_input($items) {
+		return explode(' ,', $items);
+	}
+
+?>
