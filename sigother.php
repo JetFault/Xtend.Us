0849af169d5e1bfa0c39021c70c1d96bd65c4153
Karina Ruzinov <Kruzinov@172-30-6-114.dynapool.nyu.edu>
2012-03-24 22:06:28 -0400

    Some mongo stuff

--- sigother.php
+++ sigother.php
@@ -1,9 +1,19 @@
 <?php
 
+$connection = new Mongo();
+
 		public function addLike($userID, $like) {
+			$db = $connection->$userID;
+			$collection = $db->likes;
+			$doc = array($like);
+			$collection->insert($doc);
 		}
 
 		public function addDislike($userID, $dislike) {
+			$db = $connection->$userID;
+			$collection = $db->dislikes;
+			$doc = array($dislike);
+			$collection->insert($doc);
 		}
 
 		public function getLikes($userID) {
