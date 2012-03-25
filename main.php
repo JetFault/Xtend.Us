d9fbb3989509972eb71cb3e1a6222dcb61544989
Jerry Reptak <jreptak@gmail.com>
2012-03-25 06:57:49 -0400

    bleh, no front end at all

--- main.php
+++ main.php
@@ -7,15 +7,19 @@ include("user.php");
 session_start();
 
 
-$phone = $_POST['phone'];
-$pass = $_POST['pass'];
+if(!isset($_SESSION['userid'])) {
+	if(!isset($_POST['phone']) || !isset($_POST['pass'])) {
+		header('Location: http://ruslug.rutgers.edu/~relext/');
+		return;
+	}
+	
+	$phone = $_POST['phone'];
 
-if(isset($phone) && isset($pass)) {
 	$_SESSION['userid'] = getUserIDbyPhone($phone);
 }
 
 
-if(!isset($_SESSION['userid'])) {
+if(!isset($_SESSION['userid']) || strcmp($_SESSION['userid'], "") == 0) {
 	header( 'Location: http://ruslug.rutgers.edu/~relext/');
 	return;
 }
@@ -27,6 +31,7 @@ $userID = $_SESSION['userid'];
 
 <html>
 <body>
+
 
 
 </body
