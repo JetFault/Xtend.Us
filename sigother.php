2b50a10ce32c8da77981a9a94ea9b1c4e3d83957
Jerry Reptak <jreptak@gmail.com>
2012-03-24 22:05:30 -0400

    User and sigother

--- sigother.php
+++ sigother.php
@@ -1,17 +1,29 @@
 <?php
 
-		public function addLike($userID, $like) {
-		}
+#userID is the ID of master of relationship
+#sigID  is the ID of significant other
 
-		public function addDislike($userID, $dislike) {
-		}
+public function createSigOther($userID, $sigName) {
+	/* This should create a new significant other entity.
+	 * Add that significant other to the list of significant others.
+	 * 
+	 * Only allow one active sig other.
+	 * */
+}
 
-		public function getLikes($userID) {
-			return $this->likes;
-		}
-		
-		public function getDislikes($userID) {
-			return $this->dislikes;
-		}
+public function addAnniversary($sigID, $month, $day, $year) {
+}
+
+public function addLike($sigID, $like) {
+}
+
+public function addDislike($sigID, $dislike) {
+}
+
+public function getLikes($sigID) {
+}
+
+public function getDislikes($sigID) {
+}
 
 ?>
---
+++ user.php
@@ -0,0 +1,18 @@
+<?php
+
+public function getName($userID) {
+}
+
+public function getActiveSigOther($userID) {
+}
+
+public function getAllSigOthers($userID) {
+	/* This should return a list of sigIDs.
+	 * Sorted chronologically from recent to past
+	 */
+}
+
+public function removeActiveSigOther($userID) {
+}
+
+?>
