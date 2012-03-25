ac6131491b701f3a0c130908c26c19d52d2dda26
Karina Ruzinov <Kruzinov@172-30-6-114.dynapool.nyu.edu>
2012-03-25 00:15:18 -0400

    back end as hell

--- user.php
+++ user.php
@@ -1,19 +1,36 @@
 <?php
 
+$connection = new Mongo();
+
 public function createUser($userID, $userName) {
+	$db = $connection->allusers->list;
+
+	$db->insert(array("name"=> $userName,"ID" => $userID);
 }
 
 
 public function getName($userID) {
-}
+	$db = $connection->allusers->list;
+	$result = $db->find(array('ID' => $userID));
 
+	return $result['name'];
+}
 public function getNumber($userID) {
+	$db = $connection->allusers->list;
+	$result = $db->find(array('ID' => $userID));
+
+	return $result['number'];
 }
 
 public function setNumber($userID, $number) {
+
 }
 
 public function getUserIDbyPhone($phoneNumber) {
+	$db = $connection->allusers->list;
+	$result = $db->find(array('number' => $phoneNumber));
+	
+	return $result['ID'];
 }
 
 public function getActiveSigOther($userID) {
